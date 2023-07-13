b9894da1c38af093afba04b22890a59c3cc5a2d1
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-07-13 16:54:17 +0330

    console.log removed

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,6 @@ const Home = async ({ searchParams }) => {
     model: searchParams.model || "",
   });
   const isDataRecieved = allCars.length > 0;
-  console.log(searchParams);
 
   return (
     <div>
